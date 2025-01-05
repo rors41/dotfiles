1741f9977f997e7ed236581c19eac657881ddc05
Matej Martinkovič <martinkovic.mat@gmail.com>
2025-01-05 04:01:00 +0100

    rust analyzer

--- nvim/init.lua
+++ nvim/init.lua
@@ -614,7 +614,16 @@ require('lazy').setup({
         -- clangd = {},
         gopls = {},
         pyright = {},
-        -- rust_analyzer = {},
+        rust_analyzer = {
+          settings = {
+            cargo = {
+              allFeatures = true,
+            },
+            imports = {
+              group = true,
+            },
+          },
+        },
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
